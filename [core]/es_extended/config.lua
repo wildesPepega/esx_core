f8665415cd661b6353281d93bfab1ad3a71a1eb8
wildesPepega <68642699+wildesPepega@users.noreply.github.com>
2024-02-25 03:55:31 +0100

    Update config.lua

--- [core]/es_extended/config.lua
+++ [core]/es_extended/config.lua
@@ -22,12 +22,13 @@ Config.StartingAccountMoney = { bank = 50000 }
 
 Config.StartingInventoryItems = false -- table/false
 
+--Config.StartingInventoryItems = {
+--    bread = 1,
+--    water = 1
+--}
+
 Config.DefaultSpawns = { -- If you want to have more spawn positions and select them randomly uncomment commented code or add more locations
     { x = 222.2027, y = -864.0162, z = 30.2922, heading = 1.0 },
-    --{x = 224.9865, y = -865.0871, z = 30.2922, heading = 1.0},
-    --{x = 227.8436, y = -866.0400, z = 30.2922, heading = 1.0},
-    --{x = 230.6051, y = -867.1450, z = 30.2922, heading = 1.0},
-    --{x = 233.5459, y = -868.2626, z = 30.2922, heading = 1.0}
 }
 
 Config.AdminGroups = {
@@ -45,26 +46,26 @@ Config.EnableDefaultInventory = true -- Display the default Inventory ( F2 )
 Config.EnableWantedLevel = false -- Use Normal GTA wanted Level?
 Config.EnablePVP = true -- Allow Player to player combat
 
-Config.Multichar = GetResourceState("esx_multicharacter") ~= "missing"
+Config.Multichar = false
 Config.Identity = true -- Select a characters identity data before they have loaded in (this happens by default with multichar)
 Config.DistanceGive = 4.0 -- Max distance when giving items, weapons etc.
 
 Config.AdminLogging = false -- Logs the usage of certain commands by those with group.admin ace permissions (default is false)
 
-Config.DisableHealthRegeneration = false -- Player will no longer regenerate health
-Config.DisableVehicleRewards = false -- Disables Player Recieving weapons from vehicles
-Config.DisableNPCDrops = false -- stops NPCs from dropping weapons on death
-Config.DisableDispatchServices = false -- Disable Dispatch services
-Config.DisableScenarios = false -- Disable Scenarios
+Config.DisableHealthRegeneration = true -- Player will no longer regenerate health
+Config.DisableVehicleRewards = true -- Disables Player Recieving weapons from vehicles
+Config.DisableNPCDrops = true -- stops NPCs from dropping weapons on death
+Config.DisableDispatchServices = true -- Disable Dispatch services
+Config.DisableScenarios = true -- Disable Scenarios
 Config.DisableWeaponWheel = false -- Disables default weapon wheel
-Config.DisableAimAssist = false -- disables AIM assist (mainly on controllers)
-Config.DisableVehicleSeatShuff = false -- Disables vehicle seat shuff
+Config.DisableAimAssist = true -- disables AIM assist (mainly on controllers)
+Config.DisableVehicleSeatShuff = true -- Disables vehicle seat shuff
 Config.DisableDisplayAmmo = false -- Disable ammunition display
 Config.RemoveHudComponents = {
-    [1] = false, --WANTED_STARS,
+    [1] = true, --WANTED_STARS,
     [2] = false, --WEAPON_ICON
-    [3] = false, --CASH
-    [4] = false, --MP_CASH
+    [3] = true, --CASH
+    [4] = true, --MP_CASH
     [5] = false, --MP_MESSAGE
     [6] = false, --VEHICLE_NAME
     [7] = false, -- AREA_NAME
@@ -76,22 +77,14 @@ Config.RemoveHudComponents = {
     [13] = false, --CASH_CHANGE
     [14] = false, --RETICLE
     [15] = false, --SUBTITLE_TEXT
-    [16] = false, --RADIO_STATIONS
-    [17] = false, --SAVING_GAME,
-    [18] = false, --GAME_STREAM
+    [16] = true, --RADIO_STATIONS
+    [17] = true, --SAVING_GAME,
+    [18] = true, --GAME_STREAM
     [19] = false, --WEAPON_WHEEL
     [20] = false, --WEAPON_WHEEL_STATS
     [21] = false, --HUD_COMPONENTS
     [22] = false, --HUD_WEAPONS
 }
 
-Config.SpawnVehMaxUpgrades = true -- admin vehicles spawn with max vehcle settings
-Config.CustomAIPlates = "........" -- Custom plates for AI vehicles
--- Pattern string format
---1 will lead to a random number from 0-9.
---A will lead to a random letter from A-Z.
--- . will lead to a random letter or number, with 50% probability of being either.
---^1 will lead to a literal 1 being emitted.
---^A will lead to a literal A being emitted.
---Any other character will lead to said character being emitted.
--- A string shorter than 8 characters will be padded on the right.
+Config.SpawnVehMaxUpgrades = true
+Config.CustomAIPlates = "PEEPO"
